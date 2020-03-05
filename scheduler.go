0f70534bdd9734e3f574aefd81077caa34b7b6f0
Eugene Bravov <eugene.bravov@gmail.com>
2020-03-05 22:08:46 +0300

    Add some random

--- scheduler.go
+++ scheduler.go
@@ -1,10 +1,10 @@
 package main
 
 import (
+	"math/rand"
 	"time"
 
 	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
 type Scheduler struct {
@@ -19,15 +19,13 @@ func (s *Scheduler) Schedule(user *User) {
 		return
 	}
 	s.db.Where("user_id = ?", user.ID).Unscoped().Delete(&Schedule{})
-	start := *user.StartsAt + time.Minute*15
+
+	s.scheduleAtTimeWithRandom(*user.StartsAt+time.Minute*10, user)
+	start := *user.StartsAt + time.Hour*time.Duration(*user.EveryHours)
 	till := *user.StartsAt + time.Hour*8
 
 	for {
-		schedule := Schedule{
-			UserID: user.ID,
-			At:     start,
-		}
-		s.db.Save(&schedule)
+		s.scheduleAtTimeWithRandom(start, user)
 
 		start += time.Hour * time.Duration(*user.EveryHours)
 		if start > till {
@@ -38,4 +36,22 @@ func (s *Scheduler) Schedule(user *User) {
 	user.LastWorkout = nil
 	user.LastScheduleID = nil
 	s.db.Save(user)
+}
+
+func (s *Scheduler) ResetUserSetting(user *User) {
+	user.StartsAt = nil
+	user.EveryHours = nil
+	user.LastWorkout = nil
+	user.LastScheduleID = nil
+	s.db.Save(user)
+	s.db.Where("user_id = ?", user.ID).Unscoped().Delete(&Schedule{})
+}
+
+func (s *Scheduler) scheduleAtTimeWithRandom(start time.Duration, user *User) {
+	rand.Seed(time.Now().UnixNano())
+	schedule := Schedule{
+		UserID: user.ID,
+		At:     start + time.Minute*time.Duration(rand.Intn(7)),
+	}
+	s.db.Save(&schedule)
 }
